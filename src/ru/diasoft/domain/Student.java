a39a342274f9be73252fb473ed5f121e54d41ecc
gantsevsn <gantsevsn@gmail.com>
2025-03-22 11:00:58 +0300

    Module 10

--- src/ru/diasoft/domain/Student.java
+++ src/ru/diasoft/domain/Student.java
@@ -2,15 +2,12 @@ package ru.diasoft.domain;
 
 import ru.diasoft.Exception.InvalidPhoneNumberException;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Student extends Person{
     private String group;
     private int course;
-    public Set<Subject> subjects= new TreeSet<>();
+    public Set<Subject> subjects= new TreeSet<>((o1, o2) -> o1.getName().compareTo(o2.getName()));
 
     public Student(String firstName, String secondName, int age, String phone, String group, int course) throws InvalidPhoneNumberException {
         super(firstName, secondName, age, phone);
